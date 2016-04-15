4ba6a48c29e1b529e29d2418dd4b96e53acc6b3f
Jan Meppe <janmeppe@hotmail.com>
2016-04-15 19:20:13 +0200

    Added duplicate checking

--- getbib.py
+++ getbib.py
@@ -16,14 +16,27 @@ user_arg      = sys.argv[1]
 query_unicode = gscholar.query(user_arg)
 query         = [x.encode('UTF8') for x in query_unicode]
 
-# Append bibtex output to prespecified path 
-with open(filename,'a') as out:
-    for item in query:
-        out.write("%s" % item)
-    out.write("\n") # such that the next item is well spaced 
-
-# @article{nameYEARword, ... } find cite_id between the first '{' and ','
+# @article{nameYEARword, ... find cite_id between the first '{' and ','
 cite_id = query[0].split(',', 1)[0].split('{',1)[1]
-print "Citation added succesfully!\n\cite{%s}\n\citeA{%s}" % (cite_id, cite_id)
 
+# Check for duplicates in the .bib file (filename)
+def contains_duplicate(cite_id):
+    flag = False
+    with open(filename,'r') as out:
+        for line in out: 
+            if cite_id in line:
+                flag = True
+                return flag 
+            else:
+                pass
+    return flag 
 
+if contains_duplicate(cite_id) == True:
+    print "Duplicate found, aborting."
+else:
+    # Append bibtex output to prespecified path 
+    with open(filename,'a') as out:
+        for item in query:
+            out.write("%s" % item)
+        out.write("\n") # such that the next item is well spaced 
+    print "Citation 
